453fa350fd737c6a1496668ed4e3013493c9778f
Vicente Silva Vera <ve.silvavera@gmail.com>
2022-09-15 14:48:33 -0300

    optimizacion RBT

--- Search trees/RedBlackTree.cpp
+++ Search trees/RedBlackTree.cpp
@@ -2,97 +2,98 @@
 
 using namespace std;
 
-#define red -1
-#define black 1
+//definimos constantes para mejor comprension de codigo
+#define red false
+#define black true
 
-void RedBlackTree::left_rotate(Node *n){
-	Node *n2 = n->r;
-	Node *n3 = n->r->l;
+void RedBlackTree::left_rotate(RBTNode *n){
+	RBTNode *n2 = n->r;
+	RBTNode *n3 = n->r->l;
 	//actualizamos padre del elemento 2
 	if(n == root){//reemplaza root
-		parent[n2] = n2;
+		n2->parent = n2;
 		root = n2;
 	}
-	else if(parent[n]->l == n){
-		parent[n2] = parent[n];
-		parent[n]->l = n2;
+	else if(n->parent->l == n){
+		n2->parent = n->parent;
+		n->parent->l = n2;
 	}
 	else{
-		parent[n2] = parent[n];
-		parent[n]->r = n2;
+		n2->parent = n->parent;
+		n->parent->r = n2;
 	} 
 	//ejecutamos rotacion
 	n2->l = n;
 	n->r = n3;
-	parent[n] = n2;
-	if(n3 != NULL)parent[n3] = n;
+	n->parent = n2;
+	if(n3 != nullptr)n3->parent = n;
 }
 
-void RedBlackTree::right_rotate(Node *n){
-	Node *n2 = n->l;
-	Node *n3 = n->l->r;
+void RedBlackTree::right_rotate(RBTNode *n){
+	RBTNode *n2 = n->l;
+	RBTNode *n3 = n->l->r;
 	//actualizamos padre del nodo 2
 	if(n == root){//reemplaza root
-		parent[n2] = n2;
+		n2->parent = n2;
 		root = n2;
 	}
-	else if(parent[n]->l == n){
-		parent[n2] = parent[n];
-		parent[n]->l = n2;
+	else if(n->parent->l == n){
+		n2->parent = n->parent;
+		n->parent->l = n2;
 	}
 	else{
-		parent[n2] = parent[n];
-		parent[n]->r = n2;
+		n2->parent = n->parent;
+		n->parent->r = n2;
 	}
 	//ejecutamos rotacion
 	n2->r = n;
 	n->l = n3;
-	parent[n] = n2;
-	if(n3 != NULL)parent[n3] = n;
+	n->parent = n2;
+	if(n3 != nullptr)n3->parent = n;
 }
 
-void RedBlackTree::fixInsert(Node *n){//balanceamos el arbol
-	while(color[parent[n]->key] == red){
+void RedBlackTree::fixInsert(RBTNode *n){//balanceamos el arbol
+	while(n->parent->color == red){
 		if(n == root){//caso root
-			color[n->key] = black;
+			n->color = black;
 			return;
 		}
-		Node* uncle;
-		Node* granp = parent[parent[n]];
+		RBTNode* uncle;
+		RBTNode* granp = n->parent->parent;
 
-		if(parent[n] != granp->l) uncle = granp->l;//determinamos nodo uncle
+		if(n->parent != granp->l) uncle = granp->l;//determinamos nodo uncle
 		else uncle = granp->r;
 
 		//caso uncle rojo
-		if(uncle != NULL and color[uncle->key] == red){
-			color[granp->key] = red;
-			color[parent[n]->key] = black;
-			color[uncle->key] = black;
+		if(uncle != nullptr and uncle->color == red){
+			granp->color = red;
+			n->parent->color = black;
+			uncle->color = black;
 			n = granp;
 			continue;
 		}
 		//casos black uncle
 		//caso triangulo derecho
-		else if(parent[n]->l == n and granp->r == parent[n]){
-			Node *newn = parent[n];
-			right_rotate(parent[n]);
+		else if(n->parent->l == n and granp->r == n->parent){
+			RBTNode *newn = n->parent;
+			right_rotate(n->parent);
 			n = newn;
 		}
 		//caso triangulo izquierdo
-		else if(parent[n]->r == n and granp->l == parent[n]){
-			Node *newn = parent[n];
-			left_rotate(parent[n]);
+		else if(n->parent->r == n and granp->l == n->parent){
+			RBTNode *newn = n->parent;
+			left_rotate(n->parent);
 			n = newn;
 		}
 		//caso linea hacia la derecha
-		if(parent[n]->r == n and granp->r == parent[n]){
-			color[granp->key] = red;
-			color[parent[n]->key] = black;
+		if(n->parent->r == n and granp->r == n->parent){
+			granp->color = red;
+			n->parent->color = black;
 			left_rotate(granp);
 		}
 		else{//caso linea a la izquierda
-			color[granp->key] = red;
-			color[parent[n]->key] = black;
+			granp->color = red;
+			n->parent->color = black;
 			right_rotate(granp);
 
 		}
@@ -101,54 +102,52 @@ void RedBlackTree::fixInsert(Node *n){//balanceamos el arbol
 
 }
 
-Node* RedBlackTree::createNode(int k){//override
+RBTNode* RedBlackTree::createNode(int k){//override
 	if (size == 0){//primer nodo
 		size++;
-		root = new Node(k);
-		parent[root] = root;
+		root = new RBTNode(k);
+		root->parent = root;
 		return root;
 	}
-	Node *t = root;
-	while(t != NULL){//encontramos lugar en el arbol e insertamos registrando padre
-		if(t->key == k)return NULL;
+	RBTNode *t = root;
+	while(t != nullptr){//encontramos lugar en el arbol e insertamos registrando padre
+		if(t->key == k)return nullptr;
 		if(t->key > k){
-			if(t->l != NULL){
+			if(t->l != nullptr){
 				t = t->l;
 				continue;
 			}
-			t->l = new Node(k);
-			parent[t->l] = t;
+			t->l = new RBTNode(k);
+			t->l->parent = t;
 			size++;
 			return t->l;
 		}
-		if(t->r != NULL){
+		if(t->r != nullptr){
 			t = t->r;
 			continue;
 		}
-		t->r = new Node(k);
-		parent[t->r] = t;
+		t->r = new RBTNode(k);
+		t->r->parent = t;
 		size++;
 		return t->r;
 	}
-	return NULL;
+	return nullptr;
 }
 
 void RedBlackTree::insert(int k){
-	Node *newN =  createNode(k);//creamos nodo
-	if(newN == NULL)return;//ya existia nodo en set
-	//establecemos como base colo rojo
-	color[newN->key] = red;
+	RBTNode *newN =  createNode(k);//creamos nodo
+	if(newN == nullptr)return;//ya existia nodo en set
 	fixInsert(newN);
 }
 
-void RedBlackTree::printColors(const string& prefix, const Node* node, bool isLeft)
+void RedBlackTree::printColors(const string& prefix, const RBTNode* node, bool isLeft)
 {//funcion de impresion
     if( node != nullptr )
     {
         cout << prefix;
         cout << (isLeft ? "├──" : "└──" );
         string c;
-        if(color[node->key] == 1)c = "b";
+        if(node->color == black)c = "b";
         else c = "r";
         cout << node->key <<" "<<c<< std::endl;
 
@@ -156,3 +155,26 @@ void RedBlackTree::printColors(const string& prefix, const Node* node, bool isLe
 		printColors( prefix + (isLeft ? "│   " : "    "), node->l, false);
     }
 }
+
+bool RedBlackTree::search(int k){
+	RBTNode *t = root;
+	while(t!=NULL){
+		if(t->key == k)
+			return true;
+		if(t->key > k){
+			if(t->l != NULL){
+				t = t->l;
+				continue;
+			}
+			return false;
+		}
+		if(t->r != NULL){
+			t = t->r;
+			continue;
+		}
+		return false;
+	}
+	return false;
+}
+
+RBTNode::RBTNode(int k):Node(k){}
--- Search trees/RedBlackTree.h
+++ Search trees/RedBlackTree.h
@@ -1,24 +1,27 @@
 #include "binaryTree.h"
-#include <map>
-
-#define red -1
-#define black 1 
 
 using namespace std;
 
-
+class RBTNode: public Node {
+	public:
+        RBTNode(int k);
+        RBTNode* parent = nullptr;
+        RBTNode* l = nullptr;
+        RBTNode* r = nullptr;
+        bool color = false;
+};
 
 class RedBlackTree: public BinaryTree {
 	private:
 		int size = 0;
-		map<int,int> color;//mapa que determina el color de los nodos
-		map<Node*,Node*> parent;//contiene relacion de padre e hijo entre nodos
-		Node* createNode(int k);
-		void fixInsert(Node *n);
-		void right_rotate(Node *n);
-		void left_rotate(Node *n);
+		RBTNode* createNode(int k);
+		void fixInsert(RBTNode *n);
+		void right_rotate(RBTNode *n);
+		void left_rotate(RBTNode *n);
 
 	public: 
-		void printColors(const string& prefix, const Node* node, bool isLeft);
+		RBTNode *root = nullptr;
+		bool search(int k);
+		void printColors(const string& prefix, const RBTNode* node, bool isLeft);
 		void insert(int k);
 };
